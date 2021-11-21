92f2ca238b83652fbf9f78aefea9e99393c7708a
madasamy1504 <rdiitpersoninperform@gmail.com>
2021-11-21 10:18:55 +0530

    accordion

--- 4.Accordion(workinprogress)/main.js
+++ 4.Accordion(workinprogress)/main.js
@@ -1,12 +1,11 @@
-let buttons = document.querySelectorAll('question-btn');
-    
-    function printMe(button){
-      button.addEventListener('click',function(){
-        let currentElement = this,
-            getElement = currentElement.parentNode.parentNode;
-            console.log('getElement');
+let buttons = document.querySelectorAll(".question-btn");
 
-      });
-    
-    };
-    buttons.forEach(printMe());
+function printMe(button) {
+  button.addEventListener("click", function () {
+    let currentElement = this,
+      getElement = currentElement.parentNode.parentNode,
+      displayContent = getElement.querySelector(".question-text");
+    displayContent.style.display = "block";
+  });
+}
+buttons.forEach(printMe);
